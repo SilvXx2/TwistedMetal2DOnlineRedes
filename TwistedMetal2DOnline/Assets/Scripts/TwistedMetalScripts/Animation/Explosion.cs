f10c130e2b07445b7adbbd74da87b26923fad6a2
reytom20 <reytom2004@gmail.com>
2026-07-02 14:43:24 -0300

    Animation Fixed

--- TwistedMetal2DOnline/Assets/Scripts/TwistedMetalScripts/Animation/Explosion.cs
+++ TwistedMetal2DOnline/Assets/Scripts/TwistedMetalScripts/Animation/Explosion.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 public class Explosion : MonoBehaviourPun
 {
     [SerializeField] private float destroyTime = 0.8f;
+    private Animator animator;
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
+
         // Si fue instanciado a través de la red (PhotonNetwork.Instantiate)
         if (PhotonNetwork.InRoom && photonView != null && photonView.InstantiationId > 0)
         {
             if (photonView.IsMine)
             {
+                if (animator != null)
+                {
+                    animator.SetBool("Play", true);
+                }
                 StartCoroutine(DestroyAfterTimeNetworked());
             }
         }
         else
         {
             // Si fue instanciado localmente (mediante Instantiate común de Unity)
+            if (animator != null)
+            {
+                animator.SetBool("Play", true);
+            }
             Destroy(gameObject, destroyTime);
         }
     }
